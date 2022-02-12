04d9c2e7062dccd35d66969fe50545fb33edf8a7
Igor Drobinin <idrobinin@gmail.com>
2022-02-12 16:42:24 +0800

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,91 +1,45 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Denpasar&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.denpasar-title').textContent = data.name;
-      document.querySelector('.denpasar-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.denpasar-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.denpasar-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.denpasar-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.tokyo-title').textContent = data.name;
-      document.querySelector('.tokyo-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.tokyo-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.tokyo-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.tokyo-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.amsterdam-title').textContent = data.name;
-      document.querySelector('.amsterdam-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.amsterdam-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.amsterdam-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.amsterdam-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.moscow-title').textContent = data.name;
-      document.querySelector('.moscow-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.moscow-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.moscow-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.moscow-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.madrid-title').textContent = data.name;
-      document.querySelector('.madrid-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.madrid-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.madrid-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.madrid-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=85845f8459fdd8f701377c8b9320545b')
-   .then(function (resp) { return resp.json() })
-   .then(function (data) {
-      console.log(data);
-      document.querySelector('.london-title').textContent = data.name;
-      document.querySelector('.london-temp').innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
-      document.querySelector('.london-sky').textContent = data.weather[0]['description'];
-      document.querySelector('.london-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      document.querySelector('.london-wind').innerHTML = data.wind.speed + ' m/s';
-   })
-   .catch(function () {
-      //  any errors 
-   });
-
 const hideEl = document.querySelectorAll('#hide');
 const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-   slide.addEventListener('click', () => {
-      clearActiveClasses();
-      slide.classList.add('active')
-   })
+
+const param = {
+   "url": "https://api.openweathermap.org/data/2.5/",
+   "appid": "85845f8459fdd8f701377c8b9320545b"
 }
+
+function getWeather() {
+
+   for (const slide of slides) {
+
+      slide.addEventListener('click', () => {
+         const cityName = slide.getAttribute('value');
+
+         clearActiveClasses();
+
+         slide.classList.add('active')
+
+
+         fetch(`${param.url}weather?q=${cityName}&appid=${param.appid}`)
+
+            .then(function (resp) { return resp.json() })
+            .then(function (data) {
+               console.log(data);
+               document.querySelector(`.${cityName.toLowerCase()}-title`).textContent = data.name;
+               document.querySelector(`.${cityName.toLowerCase()}-temp`).innerHTML = Math.floor(data.main.temp - 273.15) + '&deg';
+               document.querySelector(`.${cityName.toLowerCase()}-sky`).textContent = data.weather[0]['description'];
+               document.querySelector(`.${cityName.toLowerCase()}-icon`).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+               document.querySelector(`.${cityName.toLowerCase()}-wind`).innerHTML = data.wind.speed + ' m/s';
+            })
+
+      })
+   }
+
+}
+
+
+
+
+getWeather()
 
 
 function clearActiveClasses() {
@@ -93,6 +47,7 @@ function clearActiveClasses() {
       slide.classList.remove('active')
    })
 }
+
 
 
 
